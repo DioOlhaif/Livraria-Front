baa00566b34504a2de3b602f01a00202751c14e0
Dio <diovane.fialho@sga.pucminas.br>
2021-09-24 19:31:51 -0300

    Api livraria frontend

--- karma.conf.js
+++ karma.conf.js
@@ -41,4 +41,13 @@ module.exports = function (config) {
     singleRun: false,
     restartOnFileChange: true
   });
+  const cors=require("cors");
+  const corsOptions ={
+     origin:'*', 
+     credentials:true,            //access-control-allow-credentials:true
+     optionSuccessStatus:200,
+  }
+  
+  app.use(cors(corsOptions))
+
 };
